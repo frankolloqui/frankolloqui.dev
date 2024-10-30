b044caacf19d83d319462d748fe652bea0f796d9
Frank Olloqui <devolloqui@gmail.com>
2024-10-30 15:47:50 -0700

    init with nav

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Header2 from "@/components/Header2";
+// import Image from "next/image";
+
+export default function Home() {
+  return <Header2 />;
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,114 @@
+// components/Header.tsx
+
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+// import ToggleTheme from "./ToggleTheme";
+
+export default function Header() {
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setScroll(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const toTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <header className="header z-40 relative">
+      <Link
+        href="/"
+        passHref
+        className="w-12 h-12 absolute xl:fixed m-5 select-none outline-none"
+      >
+        <Image src="/home.svg" alt="Vercel logomark" width={20} height={20} />
+      </Link>
+      <button
+        title="Scroll to top"
+        className={`fixed right-3 bottom-3 w-10 h-10 rounded-full transition duration-300 z-100 print:hidden ${
+          scroll > 300
+            ? "opacity-30 hover:opacity-100"
+            : "opacity-0 pointer-events-none"
+        }`}
+        onClick={toTop}
+      >
+        <div className="text-lg font-bold">⬆</div>
+      </button>
+      <nav className="nav grid grid-cols-1 md:grid-cols-[1fr_auto] p-4">
+        <div className="spacer" />
+        <div className="right grid grid-flow-col gap-4 items-center print:opacity-0">
+          <Link
+            href="/posts"
+            className="flex items-center space-x-2"
+            title="Blog"
+          >
+            <span className="hidden md:block">Blog</span>
+            <div className="md:hidden">📰</div>
+          </Link>
+          <Link
+            href="/projects"
+            className="flex items-center space-x-2"
+            title="Projects"
+          >
+            <span className="hidden md:block">Projects</span>
+            <div className="md:hidden">💡</div>
+          </Link>
+          <Link href="/talks" className="hidden md:block" title="Talks">
+            Talks
+          </Link>
+          <Link
+            href="/sponsors-list"
+            className="flex items-center space-x-2"
+            title="Sponsors"
+          >
+            <span className="hidden md:block">Sponsors</span>
+            <div className="md:hidden">❤️</div>
+          </Link>
+          <Link href="/podcasts" className="hidden md:block" title="Podcasts">
+            🎙️
+          </Link>
+          <Link href="/demos" title="Demos">
+            📸
+          </Link>
+          <Link href="/chat" title="Let's Chat">
+            💬
+          </Link>
+          <a
+            href="https://twitter.com/antfu7"
+            target="_blank"
+            title="Twitter"
+            className="hidden md:block"
+          >
+            🐦
+          </a>
+          <a
+            href="https://github.com/antfu"
+            target="_blank"
+            title="GitHub"
+            className="hidden md:block"
+          >
+            🐱
+          </a>
+          <a
+            href="/feed.xml"
+            target="_blank"
+            title="RSS"
+            className="hidden md:block text-xl"
+          >
+            📡
+          </a>
+          <Image src="/home.svg" alt="Theme" width={20} height={20} />
+        </div>
+      </nav>
+    </header>
+  );
+}
