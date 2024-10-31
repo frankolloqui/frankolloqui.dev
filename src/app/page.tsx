8ead9d60abd5a8562e40399b2083be315587e7a7
Frank Olloqui <devolloqui@gmail.com>
2024-10-31 13:47:18 -0700

    alignment adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <div>
       <Navbar />
 
-      <div className="flex flex-col items-center justify-center h-screen px-4 sm:px-8 md:px-16">
-        <div className="max-w-2xl w-full">
+      <div className="flex flex-col items-center  h-screen px-4 sm:px-8 md:px-16">
+        <div className="max-w-2xl w-full mt-20">
           <h1 className="text-4xl font-bold mb-10">Frank Olloqui</h1>
-          <p className="text-lg">
+          <p className="text-lg text-custom-text">
             Full-stack software engineer with a passion for crafting creative,
             innovative solutions to complex challenges. Skilled in developing
             and refining web technologies to drive efficiency and impact.
