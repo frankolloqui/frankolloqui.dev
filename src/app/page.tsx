e244e250dec2746520c0cda3a4077320142df5d5
Frank Olloqui <devolloqui@gmail.com>
2024-10-31 13:12:49 -0700

    my name lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Navbar />
 
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+        <h1 className="text-4xl font-bold">FRANK OLLOQUI</h1>
         <p className="text-lg">this is a work in progress</p>
         <p className="text-lg">come back soon</p>
       </div>
