e26bf1d5a64e94b8c56e04d8bbc09a158d8ee2c1
Frank Olloqui <devolloqui@gmail.com>
2024-11-05 13:33:35 -0800

    revision about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
             </p>
 
             <p className="text-lg text-custom-text">
-              Lately, I’ve been navigating the incredible journey of being a
-              brand new dad, balancing the joys of fatherhood with the challenge
-              of finding time for my projects. On top of that, I’ve reignited my
-              passion for photography, capturing moments whenever I can.
+              Recently, I’ve embarked on the incredible journey of new
+              fatherhood, embracing its joys while continuing to make progress
+              on my projects and learning. Alongside this, I’ve rekindled my
+              passion for photography, capturing special moments as they come.
             </p>
           </section>
           <SocialLinks />
